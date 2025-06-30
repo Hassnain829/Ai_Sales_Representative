6d4b758ef1ba2062c7a881b581866d1abf633062
unknown <edwardsmith8290@gmail.com>
2025-07-01 02:38:41 +0500

    add commenting in run.py

--- run.py
+++ run.py
@@ -6,3 +6,7 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
+
+
+    '# To run the application, use the command:\n# python run.py\n'
+    '# Ensure that the .env file is properly configured with the necessary environment variables.\n'
